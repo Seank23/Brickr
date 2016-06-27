f6908078dbf6b1934a0610d449d5ea266b2a7d75
Seank23 <sean.king@gmx.com>
2016-06-27 17:22:27 +0100

    Created Prototype Main Menu UI

---
+++ Brickr/Source/Brickr/Brickr.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Brickr.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Brickr, "Brickr" );
---
+++ Brickr/Source/Brickr/BrickrGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BrickrGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BRICKR_API ABrickrGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
